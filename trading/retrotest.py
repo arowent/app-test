d01541c9af1bcaa60d13a45d6d2279f6b4011428
arowent <arowent@mail.ru>
2022-04-18 10:56:12 +0300

    update

--- trading/retrotest.py
+++ trading/retrotest.py
@@ -3,17 +3,23 @@ import argparse
 from pathlib import Path
 
 import pandas as pd
+from matplotlib import pyplot as plt
+import plotly.express as px
+from ta import trend, momentum
 
 BASE_DIR = Path(__file__).resolve().parent
 
 
 def get_dataframe_path(timeframe):
-    return Path(BASE_DIR, 'btc-usd', 'BTC-USD_{}.csv'.format(timeframe))
+    path = Path(BASE_DIR, 'btc-usd', 'BTC-USD_{}.csv'.format(timeframe))
+    data = pd.read_csv(path).drop('timestamp', axis=1)
+    return data
 
 
 def main(argv):
     if argv.timeframe is not None:
-        df_candles = pd.read_csv(get_dataframe_path(argv.timeframe)).drop('timestamp', axis=1)
+        df_candles = get_dataframe_path(argv.timeframe)
+        df_candles['ao'] = momentum.AwesomeOscillatorIndicator(df_candles['high'], df_candles['low'], 5, 34).awesome_oscillator()
         print(df_candles.tail(5))
     else:
         print('Please enter \'timeframe\'.')
@@ -23,6 +29,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='List of arguments passed to execute the program.')
     parser.add_argument('-t', '--timeframe',
-                        help='specify a \'timeframe\', for example: [15m, 1h, 1d, 1w]')
+                        help='specify a \'timeframe\', например: [15m, 1h, 1d, 1w]')
     argv = parser.parse_args()
     main(argv)
