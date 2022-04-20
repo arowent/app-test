f621cd9feb8d4e9ff96368b34db5ad646884afc9
arowent <arowent@mail.ru>
2022-04-20 12:14:42 +0300

    update

--- oop/descriptor.py
+++ oop/descriptor.py
@@ -1,10 +1,35 @@
+class Integer:
+    @classmethod
+    def verify_coord(cls, coord):
+        if type(coord) != int:
+            raise TypeError("Координата должна быть целым числом")
+
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        self.verify_coord(value)
+        setattr(instance, self.name, value)
+
+
 class Point3D:
+    x = Integer()
+    y = Integer()
+    z = Integer()
+
     def __init__(self, x, y, z) -> None:
-        self._x = x
-        self._y = y
-        self._z = z
+        self.x = x
+        self.y = y
+        self.z = z
 
     @classmethod
     def verify_coord(cls, coord):
         if type(coord) != int:
             raise TypeError("Координата должна быть целым числом")
+
+
+pt = Point3D(1, 2, 3)
+print(pt.__dict__)
