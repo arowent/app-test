2a838aff81fb45a79ae28d9d159053aa7a18d4dd
arowent <arowent@mail.ru>
2022-05-22 23:54:47 +0300

    update

--- Fluent Python/1.1.py
+++ Fluent Python/1.1.py
@@ -1,5 +1,9 @@
 import collections
-from unittest import suite
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
@@ -7,14 +11,16 @@ Card = collections.namedtuple('Card', ['rank', 'suit'])
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
-    
+
     def __init__(self) -> None:
         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        logger.info(f'Card: {self._cards}')
 
     def __len__(self):
         return len(self._cards)
 
+    def __getitem__(self, position):
+        return self._cards[position]
 
-beer_card = Card('7', 'diamonds')
-deck = FrenchDeck()
-print(beer_card)
+
+fd = FrenchDeck()
